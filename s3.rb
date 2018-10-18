f9f38dd430197babed97800aed30744e4b963337
William Dizon <wdchromium@gmail.com>
2018-10-18 11:33:47 -0700

    added descriptions to s3 methods

--- s3.rb
+++ s3.rb
@@ -1,5 +1,5 @@
-
 # module with S3 object store backend functionality
+
 module S3
   attr_writer :access_key, :secret_key, :endpoint
 
@@ -9,11 +9,13 @@ module S3
     r.bucket(@name).exists?
   end
 
+  # Create bucket if does not exist
   def s3_create_dest!
     c = Aws::S3::Client.new
     c.create_bucket(bucket: @name)
   end
 
+  # Destroy bucket after emptying contents
   def s3_destroy_dest!
     r = Aws::S3::Resource.new
     objs = s3_list_files
@@ -24,6 +26,7 @@ module S3
     c.delete_bucket(bucket: @name)
   end
 
+  # List all files in the bucket
   def s3_list_files
     require 'set'
     objs = Set.new
@@ -38,6 +41,7 @@ module S3
     return objs
   end
 
+  # Upload file FROM mineos.rb Server instance to S3 obj store
   def s3_upload_file!(env:, filename:)
     raise RuntimeError.new('parent path traversal not allowed') if filename.include? '..'
 
@@ -63,6 +67,7 @@ module S3
     obj.key #return remote objstore name
   end
 
+  # Download file TO mineos.rb Server instance from S3 obj store
   def s3_download_file!(env:, filename:)
     c = Aws::S3::Client.new
     case env
