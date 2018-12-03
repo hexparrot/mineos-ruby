a34072ec4fe10ee5f7b2ca8c8c92373eb76b1444
William Dizon <wdchromium@gmail.com>
2018-12-03 12:44:13 -0700

    added download_assets.sh for test suites

---
+++ assets/download_assets.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://launcher.mojang.com/v1/objects/b58b2ceb36e01bcd8dbf49c8fb66c55a9f0676cd/server.jar -O minecraft_server.1.8.9.jar
+
+wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.7.0.13.zip
+unzip bedrock-server-1.7.0.13.zip -d mcbe
