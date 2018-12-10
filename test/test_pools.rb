5bb6794a7863fda50865494a37782880f8e27fad
William Dizon <wdchromium@gmail.com>
2018-12-10 11:22:51 -0700

    Pool create (#12)
    * users.rb for future pool creation
    
    * added remove_user
    
    * remove_user also removes /home/user dir
    
    * replaced system call with builtin dir del
    
    * added checks for nonexistent/duplicate users
    
    * applying regex to usernames, prefix with _
    
    * limits returned users only to those matching regex
    
    * renamed Users.rb to Pools.rb for clarity
    
    Pools are groups of servers on a single hostname.  Users is
    deprecated because users already refers to users for the webui,
    and a pool shares much of the same risk, e.g., if the worker dies
    then the shared pool servers also lose STD{IN/OUT/ERR}.
    
    A linux user is still used, but due to it's heavily structured
    nature (tightly fitting regex), its importance as being seen as
    a "user" is diminished.
    
    * added poolname length limit at longer than 20
    
    * changed /etc/password reading to use 'etc' module
    
    * ensure usergroup deleted on remove_pool

---
+++ test/test_pools.rb
@@ -0,0 +1,122 @@
+require_relative '../pools'
+require 'minitest/autorun'
+
+class PoolTest < Minitest::Test
+
+  def setup
+    @inst = Pools.new
+    @pool = '_throwaway-500'
+    @pool_home = "/home/#{@pool}"
+  end
+
+  def teardown
+    system "userdel -f #{@pool} 2>/dev/null"
+    system "rm -rf #{@pool_home} 2>/dev/null"
+  end
+
+  def test_list_pools
+    require 'set'
+
+    all_pools = @inst.list_pools
+    assert_instance_of(Set, all_pools)
+    all_pools.each do |u|
+      assert_instance_of(String, u)
+      assert(u.match(Pools::VALID_NAME_REGEX))
+    end
+  end
+
+  def test_create_pool
+    before_pools = @inst.list_pools
+    assert(!before_pools.find { |u| u == @pool })
+    assert !Dir.exist?(@pool_home)
+
+    success = @inst.create_pool(@pool, 'mypassword')
+    assert(success)
+    assert Dir.exist?(@pool_home)
+
+    after_pools = @inst.list_pools
+    diff = after_pools - before_pools
+    assert_equal(@pool, diff.first)
+    assert_equal(1, diff.length) 
+  end
+
+  def test_remove_pool
+    @inst.create_pool(@pool, 'mypassword')
+    assert Dir.exist?(@pool_home)
+    before_pools = @inst.list_pools
+    assert(before_pools.include?(@pool))
+
+    @inst.remove_pool(@pool)
+    assert !Dir.exist?(@pool_home)
+    after_pools = @inst.list_pools
+    assert(!after_pools.include?(@pool))
+
+    diff = before_pools - after_pools
+    assert_equal(@pool, diff.first)
+    assert_equal(1, diff.length) 
+  end
+
+  def test_create_duplicate_pool
+    @inst.create_pool(@pool, 'mypassword')
+
+    ex = assert_raises(RuntimeError) { @inst.create_pool(@pool, "mypassword") }
+    assert_equal('pool already exists, aborting creation', ex.message)
+  end
+
+  def test_remove_invalid_pool
+    @inst.create_pool(@pool, 'mypassword')
+
+    ex = assert_raises(RuntimeError) { @inst.remove_pool("dinosaur") }
+    assert_equal('pool not found, aborting removal', ex.message)
+  end
+
+  def test_limit_poolname_to_regex
+    invalid_names = ["will", "pool", "HELLO", "MY_NAME_IS", "43242342", "_myname55", "_55-4543", "_abcdefghijklmnopq-A"]
+    invalid_names.each do |i|
+      ex = assert_raises(RuntimeError) { @inst.create_pool(i, "password") }
+      assert_equal('poolname does not fit allowable regex, aborting creation', ex.message)
+    end
+  end
+
+  def test_poolname_length
+    # due to test overlap, length checked first!
+    invalid_names = ["_abcdefghijklmnopq-12", "abcdefghijklmnopqrstuvwxyz"]
+    invalid_names.each do |i|
+      ex = assert_raises(RuntimeError) { @inst.create_pool(i, "mypassword") }
+      assert_equal('poolname is too long; limit is 20 characters', ex.message)
+    end
+  end
+
+  def test_pool_usergroup_created_and_deleted
+    require 'set'
+    require 'etc'
+
+    before_groups = Set.new
+    while e = Etc.getgrent do
+      before_groups << e[:name] if e[:name].match(Pools::VALID_NAME_REGEX)
+    end
+    Etc.endgrent
+
+    @inst.create_pool(@pool, 'mypassword')
+
+    after_groups = Set.new
+    while e = Etc.getgrent do
+      after_groups << e[:name] if e[:name].match(Pools::VALID_NAME_REGEX)
+    end
+    Etc.endgrent
+
+    diff = after_groups - before_groups
+    assert_equal(1, diff.length) 
+
+    @inst.remove_pool(@pool)
+
+    end_groups = Set.new
+    while e = Etc.getgrent do
+      end_groups << e[:name] if e[:name].match(Pools::VALID_NAME_REGEX)
+    end
+    Etc.endgrent
+
+    assert_equal(end_groups.length, before_groups.length)
+  end
+end
+
