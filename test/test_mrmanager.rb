8d7d8e27e53ffb4a4be7a769415fe088e93f58be
William Dizon <wdchromium@gmail.com>
2018-12-11 08:35:13 -0700

    initial mrmanager service

--- test/test_mrmanager.rb
+++ test/test_mrmanager.rb
@@ -59,4 +59,63 @@ class ManagerTest < Minitest::Test
     end
     assert_equal(1, step)
   end
+
+  def test_spawn_worker
+    guid = SecureRandom.uuid
+    step = 0
+
+    EM.run do
+      @ch
+      .queue('')
+      .bind(@exchange_dir, :routing_key => "to_hq")
+      .subscribe do |delivery_info, metadata, payload|
+        parsed = JSON.parse payload
+        case metadata[:headers]['directive']
+        when 'SPAWN'
+          assert_equal(@@hostname, parsed['host'])
+          assert_equal(guid, metadata.correlation_id)
+          assert_equal('receipt.directive', metadata.type)
+          assert_equal('SPAWN', metadata[:headers]['directive'])
+          assert_equal(@@hostname, metadata[:headers]['hostname'])
+          assert_equal(@@workerpool, metadata[:headers]['workerpool'])
+          assert(metadata.timestamp)
+          assert(metadata.message_id)
+          step += 1
+        when 'IDENT'
+          if metadata[:headers]['workerpool'] then
+            # coming from spawned process
+            assert_equal(@@hostname, parsed['host'])
+            assert_equal(@@workerpool, parsed['workerpool'])
+            #assert_equal(guid, metadata.correlation_id)
+            assert_equal('receipt.directive', metadata.type)
+            assert_equal('IDENT', metadata[:headers]['directive'])
+            assert_equal(@@hostname, metadata[:headers]['hostname'])
+            assert_equal(@@workerpool, metadata[:headers]['workerpool'])
+            assert(metadata.timestamp)
+            assert(metadata.message_id)
+            step += 1
+          else
+            # coming from manager
+            assert_equal(@@hostname, parsed['host'])
+            assert_equal(@@workerpool, parsed['workerpool'])
+            assert(parsed['pid'])
+            assert_equal(guid, metadata.correlation_id)
+            assert_equal('receipt.directive', metadata.type)
+            assert_equal('IDENT', metadata[:headers]['directive'])
+            assert_equal(@@hostname, metadata[:headers]['hostname'])
+            assert(metadata.timestamp)
+            assert(metadata.message_id)
+          end
+          EM.stop if step >= 2
+        end
+      end
+
+      @exchange_dir.publish({SPAWN: {workerpool: @@workerpool}}.to_json,
+                            :routing_key => "to_managers.#{@@hostname}",
+                            :type => "directive",
+                            :message_id => guid,
+                            :timestamp => Time.now.to_i)
+    end
+    assert_equal(2, step)
+  end
 end
