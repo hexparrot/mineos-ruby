d9cbcacfaa9be773b89cd3befbded0921c03ce8b
William Dizon <wdchromium@gmail.com>
2018-12-11 10:08:58 -0700

    Mrmanager (#13)
    * initial commit for manager, IDENT reply
    
    * initial mrmanager service
    
    * mrmanager can spawn subworkers
    
    big issue from before was using ENV['USER'] which didn't change
    even after process gid/egid changed.
    
    * manager copies over git repo to new workerpool users
    
    TODO: check ownership of copied files
    TODO: ensure no hardcoded paths like /home/user
    
    * copied git repo now carries new users ownership
    
    * removed hardcoded paths in favor of actual user

---
+++ mrmanager.rb
@@ -0,0 +1,118 @@
+require 'json'
+require 'eventmachine'
+require 'securerandom'
+
+EM.run do
+  hostname = Socket.gethostname
+
+  require 'yaml'
+  SECRETS_PATH = File.join(File.dirname(__FILE__), 'config', 'secrets.yml')
+  mineos_config = YAML::load_file(SECRETS_PATH)
+
+  require 'bunny'
+  conn = Bunny.new(:host => mineos_config['rabbitmq']['host'],
+                   :port => mineos_config['rabbitmq']['port'],
+                   :user => mineos_config['rabbitmq']['user'],
+                   :pass => mineos_config['rabbitmq']['pass'],
+                   :vhost => mineos_config['rabbitmq']['vhost'])
+  conn.start
+
+  ch = conn.create_channel
+  exchange_dir = ch.topic("directives")
+
+  directive_handler = lambda { |delivery_info, metadata, payload|
+    case payload
+    when 'IDENT'
+      exchange_dir.publish({ host: hostname }.to_json,
+                           :routing_key => "to_hq",
+                           :timestamp => Time.now.to_i,
+                           :type => 'receipt.directive',
+                           :correlation_id => metadata[:message_id],
+                           :headers => { hostname: hostname,
+                                         directive: 'IDENT' },
+                           :message_id => SecureRandom.uuid)
+    else
+      json_in = JSON.parse payload
+      if json_in.key?('SPAWN') then
+        fp = File.dirname(__FILE__)
+
+        def as_user(user, filepath, &block)
+          # http://brizzled.clapper.org/blog/2011/01/01/running-a-ruby-block-as-another-user/
+          begin
+            require 'etc'
+            # Find the user in the password database.
+            u = (user.is_a? Integer) ? Etc.getpwuid(user) : Etc.getpwnam(user)
+          rescue ArgumentError => e
+            #invalid name caught here
+            require_relative 'pools'
+            pool_inst = Pools.new
+            pool_inst.create_pool(user, 'mypassword')
+            retry
+          else
+            require 'fileutils'
+            FileUtils.cp_r filepath, "/home/#{user}/"
+            FileUtils.chown_R user, user, "/home/#{user}/"
+          end
+
+          # Fork the child process. Process.fork will run a given block of code
+          # in the child process.
+          Process.fork do
+            # We're in the child. Set the process's user ID.
+            Process.gid = Process.egid = u.uid
+            Process.uid = Process.euid = u.uid
+
+            # Invoke the caller's block of code.
+            Dir.chdir("/home/#{user}/mineos-ruby") do
+              block.call()
+            end
+          end
+        end
+
+        worker = json_in['SPAWN']['workerpool']
+        as_user(worker, fp) do
+          exec "ruby worker.rb"
+        end
+
+        pid = 5
+
+        exchange_dir.publish({ host: hostname,
+                               workerpool: worker,
+                               pid: 5 }.to_json,
+                             :routing_key => "to_hq",
+                             :timestamp => Time.now.to_i,
+                             :type => 'receipt.directive',
+                             :correlation_id => metadata[:message_id],
+                             :headers => { hostname: hostname,
+                                           workerpool: worker, 
+                                           directive: 'SPAWN' },
+                             :message_id => SecureRandom.uuid)
+
+      end #if
+    end #case
+  } #end directive_handler
+
+  ch
+  .queue('')
+  .bind(exchange_dir, routing_key: "to_managers.#{hostname}")
+  .subscribe do |delivery_info, metadata, payload|
+    directive_handler.call delivery_info, metadata, payload
+  end
+
+  ch
+  .queue('')
+  .bind(exchange_dir, routing_key: "to_managers")
+  .subscribe do |delivery_info, metadata, payload|
+    directive_handler.call delivery_info, metadata, payload
+  end
+
+  exchange_dir.publish({ host: hostname }.to_json,
+                       :routing_key => "to_hq",
+                       :timestamp => Time.now.to_i,
+                       :type => 'receipt.directive',
+                       :correlation_id => nil,
+                       :headers => { hostname: hostname,
+                                     directive: 'IDENT' },
+                       :message_id => SecureRandom.uuid)
+
+end #EM::Run
+
---
+++ test/test_mrmanager.rb
@@ -0,0 +1,192 @@
+require 'minitest/autorun'
+require 'eventmachine'
+require 'bunny'
+require 'json'
+require 'securerandom'
+
+class ManagerTest < Minitest::Test
+
+  def setup
+    # this test assumes a manager instance is up, as a separate process
+    @@hostname = Socket.gethostname
+    @@workerpool = 'user'
+
+    require 'yaml'
+    mineos_config = YAML::load_file('config/secrets.yml')
+
+    require 'bunny'
+    conn = Bunny.new(:host => mineos_config['rabbitmq']['host'],
+                     :port => mineos_config['rabbitmq']['port'],
+                     :user => mineos_config['rabbitmq']['user'],
+                     :pass => mineos_config['rabbitmq']['pass'],
+                     :vhost => mineos_config['rabbitmq']['vhost'])
+    conn.start
+
+    @ch = conn.create_channel
+    @exchange_dir = @ch.topic('directives')
+  end
+
+  def teardown
+    sleep(0.3)
+  end
+
+  def test_ident
+    guid = SecureRandom.uuid
+    step = 0
+
+    EM.run do
+      @ch
+      .queue('')
+      .bind(@exchange_dir, :routing_key => "to_hq")
+      .subscribe do |delivery_info, metadata, payload|
+        parsed = JSON.parse payload
+        assert_equal(@@hostname, parsed['host'])
+        assert_equal(guid, metadata.correlation_id)
+        assert_equal('receipt.directive', metadata.type)
+        assert_equal('IDENT', metadata[:headers]['directive'])
+        assert_equal(@@hostname, metadata[:headers]['hostname'])
+        assert(metadata.timestamp)
+        assert(metadata.message_id)
+        step += 1
+        EM.stop
+      end
+
+      @exchange_dir.publish('IDENT',
+                            :routing_key => "to_managers",
+                            :type => "directive",
+                            :message_id => guid,
+                            :timestamp => Time.now.to_i)
+    end
+    assert_equal(1, step)
+  end
+
+  def test_spawn_worker
+    guid = SecureRandom.uuid
+    step = 0
+
+    EM.run do
+      @ch
+      .queue('')
+      .bind(@exchange_dir, :routing_key => "to_hq")
+      .subscribe do |delivery_info, metadata, payload|
+        parsed = JSON.parse payload
+        case metadata[:headers]['directive']
+        when 'SPAWN'
+          assert_equal(@@hostname, parsed['host'])
+          assert_equal(guid, metadata.correlation_id)
+          assert_equal('receipt.directive', metadata.type)
+          assert_equal('SPAWN', metadata[:headers]['directive'])
+          assert_equal(@@hostname, metadata[:headers]['hostname'])
+          assert_equal(@@workerpool, metadata[:headers]['workerpool'])
+          assert(metadata.timestamp)
+          assert(metadata.message_id)
+          step += 1
+        when 'IDENT'
+          if metadata[:headers]['workerpool'] then
+            # coming from spawned process
+            assert_equal(@@hostname, parsed['host'])
+            assert_equal(@@workerpool, parsed['workerpool'])
+            assert_equal('receipt.directive', metadata.type)
+            assert_equal('IDENT', metadata[:headers]['directive'])
+            assert_equal(@@hostname, metadata[:headers]['hostname'])
+            assert_equal(@@workerpool, metadata[:headers]['workerpool'])
+            assert(metadata.timestamp)
+            assert(metadata.message_id)
+            step += 1
+          else
+            # coming from manager
+            assert_equal(@@hostname, parsed['host'])
+            assert_equal(@@workerpool, parsed['workerpool'])
+            assert(parsed['pid'])
+            assert_equal(guid, metadata.correlation_id)
+            assert_equal('receipt.directive', metadata.type)
+            assert_equal('IDENT', metadata[:headers]['directive'])
+            assert_equal(@@hostname, metadata[:headers]['hostname'])
+            assert(metadata.timestamp)
+            assert(metadata.message_id)
+          end
+          EM.stop if step >= 2
+        end
+      end
+
+      @exchange_dir.publish({SPAWN: {workerpool: @@workerpool}}.to_json,
+                            :routing_key => "to_managers.#{@@hostname}",
+                            :type => "directive",
+                            :message_id => guid,
+                            :timestamp => Time.now.to_i)
+    end
+    assert_equal(2, step)
+  end
+
+  def test_spawn_worker_for_new_user
+    new_user = '_throwaway-500'
+    guid = SecureRandom.uuid
+    step = 0
+
+    EM.run do
+      @ch
+      .queue('')
+      .bind(@exchange_dir, :routing_key => "to_hq")
+      .subscribe do |delivery_info, metadata, payload|
+        parsed = JSON.parse payload
+        case metadata[:headers]['directive']
+        when 'SPAWN'
+          assert_equal(@@hostname, parsed['host'])
+          assert_equal(guid, metadata.correlation_id)
+          assert_equal('receipt.directive', metadata.type)
+          assert_equal('SPAWN', metadata[:headers]['directive'])
+          assert_equal(@@hostname, metadata[:headers]['hostname'])
+          assert_equal(new_user, metadata[:headers]['workerpool'])
+          assert(metadata.timestamp)
+          assert(metadata.message_id)
+          step += 1
+        when 'IDENT'
+          if metadata[:headers]['workerpool'] then
+            # coming from spawned process
+            assert_equal(@@hostname, parsed['host'])
+            assert_equal(new_user, parsed['workerpool'])
+            assert_equal('receipt.directive', metadata.type)
+            assert_equal('IDENT', metadata[:headers]['directive'])
+            assert_equal(@@hostname, metadata[:headers]['hostname'])
+            assert_equal(new_user, metadata[:headers]['workerpool'])
+            assert(metadata.timestamp)
+            assert(metadata.message_id)
+            step += 1
+          else
+            # coming from manager
+            assert_equal(@@hostname, parsed['host'])
+            assert_equal(new_user, parsed['workerpool'])
+            assert(parsed['pid'])
+            assert_equal(guid, metadata.correlation_id)
+            assert_equal('receipt.directive', metadata.type)
+            assert_equal('IDENT', metadata[:headers]['directive'])
+            assert_equal(@@hostname, metadata[:headers]['hostname'])
+            assert(metadata.timestamp)
+            assert(metadata.message_id)
+          end
+          EM.stop if step >= 2
+        end
+      end
+
+      @exchange_dir.publish({SPAWN: {workerpool: new_user}}.to_json,
+                            :routing_key => "to_managers.#{@@hostname}",
+                            :type => "directive",
+                            :message_id => guid,
+                            :timestamp => Time.now.to_i)
+    end
+    assert_equal(2, step)
+
+    new_home = "/home/#{new_user}"
+    script_home = File.join(new_home, "mineos-ruby")
+    uid = File.stat(new_home).uid
+    owner_name = Etc.getpwuid(uid).name
+
+    assert_equal(new_user, owner_name)
+    Dir.foreach(script_home) do |item|
+      next if item == '.' or item == '..'
+      fp = File.join(script_home, item)
+      file_uid = File.stat(fp).uid
+      assert_equal(new_user, Etc.getpwuid(file_uid).name)
+    end
+  end
+end
