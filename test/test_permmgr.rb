137b288ec41b2787803e2da6b40093438653eac5
William Dizon <wdchromium@gmail.com>
2019-02-05 09:20:33 -0700

    pool perm testing

--- test/test_permmgr.rb
+++ test/test_permmgr.rb
@@ -74,11 +74,6 @@ class PermManagerTest < Minitest::Test
     # plain:user is owner and grantor? but not granted everything
     # can self-grant, though, because owner can do that.
 
-    assert(!inst.perms[:root].test_permission(user, 'mkgrantor'))
-    assert(!inst.perms[:root].test_permission(user, 'rmgrantor'))
-    assert(!inst.perms[:root].test_permission(user, 'grantall'))
-    assert(!inst.perms[:root].test_permission(user, 'revokeall'))
-
     assert(!inst.perms[:root].test_permission(user, 'mkpool'))
     assert(!inst.perms[:root].test_permission(user, 'rmpool'))
     assert(!inst.perms[:root].test_permission(user, 'spawnpool'))
@@ -115,11 +110,6 @@ class PermManagerTest < Minitest::Test
     assert(!inst.perms[:root].test_permission(user2, 'grantall'))
     assert(!inst.perms[:root].test_permission(user2, 'revokeall'))
 
-    assert(!inst.perms[:root].test_permission(user2, 'mkgrantor'))
-    assert(!inst.perms[:root].test_permission(user2, 'rmgrantor'))
-    assert(!inst.perms[:root].test_permission(user2, 'grantall'))
-    assert(!inst.perms[:root].test_permission(user2, 'revokeall'))
-
     assert(!inst.perms[:root].test_permission(user2, 'mkpool'))
     assert(!inst.perms[:root].test_permission(user2, 'rmpool'))
     assert(!inst.perms[:root].test_permission(user2, 'spawnpool'))
@@ -143,4 +133,92 @@ class PermManagerTest < Minitest::Test
     assert_equal("PERMS: CREATED PERMSCREEN `#{@workerpool} => managers.#{@hostname}`", inst2.logs.shift.message)
     assert_equal("MANAGER: MKPOOL `#{@workerpool} => managers.#{@hostname}`", inst2.logs.shift.message)
   end
+
+  def test_incremental_granting_of_pool_permissions
+    user = 'plain:user'
+    inst = PermManager.new(user)
+
+    cmd = { hostname: @hostname,
+            workerpool: @workerpool,
+            root_cmd: 'mkpool' }.to_json
+
+    inst.root_perms('grantall', user)
+    inst.root_command(JSON.parse(cmd)) { |amqp_data, rk|
+      assert_equal(@workerpool, amqp_data[:MKPOOL][:workerpool])
+      assert_equal("managers.#{@hostname}", rk)
+    }
+
+    inst.logs.clear # from root_command, not needing to be tested here
+    pool_fqdn = "#{@hostname}.#{@workerpool}"
+
+    assert(inst.perms[pool_fqdn].grantor?(user))
+    assert(inst.perms[pool_fqdn].test_permission(user, 'create'))
+    assert(inst.perms[pool_fqdn].test_permission(user, 'delete'))
+    # plain:user is owner and grantor? of pool_fqdn
+    # can self-grant, though, because owner can do that.
+
+    inst.pool_perms('grantall', user, pool_fqdn)
+    assert_equal("PERMS: #{user} granting #{user} `#{pool_fqdn}`:all", inst.logs.shift.message)
+    assert_equal("PERMS: (:all) create, delete", inst.logs.shift.message)
+
+    servername = 'testx'
+    cmd = { hostname: @hostname,
+            workerpool: @workerpool,
+            server_name: servername,
+            pool_cmd: 'create' }.to_json
+
+    inst.pool_command(JSON.parse(cmd)) { |amqp_data, rk|
+      assert_equal(servername, amqp_data["server_name"])
+      assert_equal("workers.#{@hostname}.#{@workerpool}", rk)
+    }
+
+    assert_equal("PERMS: create by #{user}@#{@hostname}.#{@workerpool}: OK", inst.logs.shift.message)
+    assert_equal("POOL: CREATE SERVER `#{servername} => workers.#{@hostname}.#{@workerpool}`", inst.logs.shift.message)
+  end
+
+  def test_incremental_granting_of_pool_permissions_for_non_owner
+    user = 'plain:user'
+    inst = PermManager.new(user)
+    user2 = 'plain:user2'
+    inst2 = PermManager.new(user2)
+
+    cmd = { hostname: @hostname,
+            workerpool: @workerpool,
+            root_cmd: 'mkpool' }.to_json
+
+    # user will make the pool so user--not user2--retains ownership
+    inst.root_perms('grantall', user)
+    inst.root_command(JSON.parse(cmd)) { |amqp_data, rk|
+      assert_equal(@workerpool, amqp_data[:MKPOOL][:workerpool])
+      assert_equal("managers.#{@hostname}", rk)
+    }
+
+    inst.logs.clear # from root_command, not needing to be tested here
+    pool_fqdn = "#{@hostname}.#{@workerpool}"
+
+    # user is doing the granting here of :all, but not grantor? status
+    inst.pool_perms('grantall', user2, pool_fqdn)
+    assert_equal("PERMS: #{user} granting #{user2} `#{pool_fqdn}`:all", inst.logs.shift.message)
+    assert_equal("PERMS: (:all) create, delete", inst.logs.shift.message)
+
+    # not a grantor, but can still create and delete
+    assert(!inst2.perms[pool_fqdn].grantor?(user2))
+    assert(inst2.perms[pool_fqdn].test_permission(user2, 'create'))
+    assert(inst2.perms[pool_fqdn].test_permission(user2, 'delete'))
+
+    servername = 'testx'
+    cmd = { hostname: @hostname,
+            workerpool: @workerpool,
+            server_name: servername,
+            pool_cmd: 'create' }.to_json
+
+    inst2.pool_command(JSON.parse(cmd)) { |amqp_data, rk|
+      assert_equal(servername, amqp_data["server_name"])
+      assert_equal("workers.#{@hostname}.#{@workerpool}", rk)
+    }
+
+    assert_equal("PERMS: create by #{user2}@#{@hostname}.#{@workerpool}: OK", inst2.logs.shift.message)
+    assert_equal("POOL: CREATE SERVER `#{servername} => workers.#{@hostname}.#{@workerpool}`", inst2.logs.shift.message)
+  end
 end
+
